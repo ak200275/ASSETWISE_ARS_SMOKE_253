bafa3cfdd8be0a1a2dd3656f681da542ca838f48
Anuj <anuj.singh@bentley.com>
2016-12-15 18:48:26 +0530

    added Start

---
+++ start.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package arsFramework;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import ARS_Pages.home_page_links;
+
+/**
+ * @author Anuj.Singh
+ *
+ */
+public class start {
+
+	/**
+	 * @param args
+	 */
+	
+	public static String repository;
+    public static String username;
+    public static String password;	      
+    public static String url;
+    public static String testcase;
+    public static Date TimeNow;
+    
+    
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+        //System.out.println("TEST CASE 1: Log-in");
+        url =  "http://pghdemoarsweb01/ars_demo";                                                //args[0];
+        username =  "admin";                                           //args[1];
+        password =  "admin";                                             //args[2];
+        repository =  "ARS_DEMO_ORACLE";                                             //args[3];
+        
+        ReadMe.readMe();
+        Date date = new Date();
+		String datetime= new SimpleDateFormat("ddMMMyy_hhmm").format(date);
+        String Reportname="C:\\ARS_Test\\ARSTestReports\\ARS_SMOKETEST_REPORT_"+datetime+".html";
+        System.out.println(Reportname);
+
+        
+        testcase="Log In Test";
+        TestcaseTemplate.testCaseTemplate(testcase);
+        
+        ExtentReports report;
+
+        report=new ExtentReports(Reportname);
+        ExtentTest extst;
+        extst=report.startTest(testcase);
+
+        TimeNow = new Date();
+        System.out.println(TimeNow);
+
+        //String gekoLocation = "C:\\ARS_Test\\GECKODRIVER\\geckodriver0110win64\\geckodriver.exe";
+		 	//System.setProperty("webdriver.firefox.marionette", gekoLocation);     
+        WebDriver driver = new FirefoxDriver();
+        
+      //String chromeLocation = "C:\\webdrivers\\Chrome\\chromedriver.exe";
+		 	//System.setProperty("webdriver.chrome.driver", chromeLocation);
+       //WebDriver driver = new ChromeDriver();
+        
+        //driver.get(url);
+        driver.navigate().to(url);
+        //WebDriver driver;
+        driver.manage().window().maximize();
+        System.out.println("Page is Maximized");
+        	   
+ 	// Log - in page     	   
+        	   
+        	   System.out.println("This is Smoke Test just to check login and the links on the subsequent page");
+        	   
+        	   System.out.println("Testing url is : " + url);
+
+        	   Actions1 ac = new Actions1(driver);
+        		String step="Check if the text box for password is displayed";
+        		String xpth = ".//*[@id='txtPassword']";
+        		System.out.println(step);
+        		if(ac.displayed(driver, xpth, step)){ 
+        			ars_logIn2 loginPage = new ars_logIn2(driver);
+        		  loginPage.logIn(repository, username, password);
+        		  	 }
+        		else{System.out.println("The page could not be displayed properly");
+        		}
+
+        		step="Check if the page loaded completely";
+        		xpth = ".//*[.='Legal Notices']";
+        		if(ac.displayed(driver, xpth, step)){ 
+        			System.out.println("The page loaded properly");
+        			extst.log(LogStatus.PASS, testcase);
+        			report.endTest(extst);
+        			report.flush();
+        		  	 }
+        		else{System.out.println("The page could not be loaded properly");
+        		extst.log(LogStatus.FAIL, testcase);
+        		report.endTest(extst);
+        		report.flush();
+        		}
+        	               	   
+        	
+        	   
+//driver.quit();
+//driver.quit();
+		
+//driver=newLogin();
+waiting.pleaseWait(7);
+System.out.println(url);
+	home_page_links homePage=new home_page_links(driver);
+	System.out.println("homepage object is created");
+	homePage.checkLinks();
+	homePage.checkMenulinks();
+	System.out.println("----------------------Completed Checking links on home page-------------------");
+		
+		
+
+	}
+
+}
